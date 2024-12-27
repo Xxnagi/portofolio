eea45eb415f59fa62bc7808efa153a8f587a8b36
Adilatul Alia <adila@Adilas-M1.local>
2024-12-27 20:42:33 +0700

    about page

--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -1,24 +1,55 @@
 import { motion } from "framer-motion";
 
-const anim = {
-  initial: {
+const defaultAnimation = {
+  hidden: {
     opacity: 0,
+    y: 20,
   },
-  animate: {
+  visible: {
     opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
   },
 };
 
-const AnimatedText = ({ text }) => {
+const AnimatedText = ({ text, className, onComplete }) => {
+  const textArray = Array.isArray(text) ? text : [text];
   return (
     <div>
       <span className="sr-only">{text}</span>
-      <motion.span className="aria-hidden">
-        {text.split("").map((char) => (
-          <motion.span>{char}</motion.span>
+      <motion.span
+        initial="hidden"
+        animate="visible"
+        onAnimationComplete={onComplete} // Trigger callback when animation finishes
+        transition={{
+          delayChildren: 1,
+          staggerChildren: 0.1,
+        }}
+        className={`aria-hidden ${className}`}
+      >
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={lineIndex}>
+            {line.split(" ").map((word, wordIndex) => (
+              <span className="inline-block" key={wordIndex}>
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={charIndex}
+                    className="inline-block"
+                    variants={defaultAnimation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </span>
+            ))}
+          </span>
         ))}
       </motion.span>
     </div>
   );
 };
+
 export default AnimatedText;
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import Curve from "../../components/Layout/Curve";
 import AnimatedText from "@/components/AnimatedText";
 
-const index = () => {
+const Index = () => {
+  const [firstAnimationComplete, setFirstAnimationComplete] = useState(false);
+
   return (
     <>
       <Curve>
-        <AnimatedText text="test" />
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+          <AnimatedText
+            text="Mustafa Ali"
+            className="text-7xl font-bold"
+            onComplete={() => setFirstAnimationComplete(true)}
+          />
+          {firstAnimationComplete && (
+            <AnimatedText
+              text={[
+                "This is written on",
+                "a typing machine. Tick tick",
+                "tick tack tack...",
+              ]}
+              className="text-2xl"
+            />
+          )}
+        </div>
       </Curve>
     </>
   );
 };
-export default index;
+
+export default Index;
